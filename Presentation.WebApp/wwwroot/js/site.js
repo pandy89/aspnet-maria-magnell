cbdfa3b5455332f15cd31584b4baaae128a63bbc
Maria Magnell <maria.magnell@gmail.com>
2026-03-31 11:23:12 +0200

    Adding FAQ section

--- Presentation.WebApp/wwwroot/js/site.js
+++ Presentation.WebApp/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿
+/* FAQ */
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) =>
+    question.addEventListener('click', () => {
+        if (question.parentNode.classList.contains('active')) {
+            question.parentNode.classList.toggle('active');
+        } else {
+            questions.forEach((q) => q.parentNode.classList.remove('active'));
+            question.parentNode.classList.add('active');
+        }
+    })
+);
